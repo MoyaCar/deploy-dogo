8e3ab7013e7a3d936a5a1a9fde43705e14c8ada8
Moy <carlosmoya@gmail.com>
2020-01-10 12:45:30 -0300

    link en logo

--- src/componentes/Navbar/Navbar.js
+++ src/componentes/Navbar/Navbar.js
@@ -5,7 +5,9 @@ function Navbar() {
  return ( 
   <div>
     <nav class="navbar fixed-top navbar-expand-lg navbar-light navbe text-white">
-      <img src={Logo} class="navbar-brand img-fluid logo" href="#recuadro-empresa"/>
+      <a href='#recuadro-empresa'>
+        <img src={Logo} class="navbar-brand img-fluid logo" href="#recuadro-empresa" alt='Logo'/>
+      </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
